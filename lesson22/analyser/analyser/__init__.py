b7706a9168bf9cfb61725ce2585c2d3e02977ffd
VitekIvZ <viktor.zaichenko@ukr.net>
2025-02-19 11:30:53 +0200

    3 commit

--- lesson22/analyser/analyser/__init__.py
+++
@@ -1,3 +0,0 @@
-from .text_handler import TextHandler
-from .dict_handler import DictHandler
-from .word_container import WordContainer
--- lesson25/nodes.py
+++
@@ -1,4 +0,0 @@
-class Node:
-    def __init__(self, data=None, next_node=None):
-        self.data = data
-        self.next_node = next_node
